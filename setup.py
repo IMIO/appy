6271ccf92340f374a0dc6a2450c609119e03159c
jdumont <jdumont@localhost>
2011-09-20 11:29:32 +0000

    updated appy to the latest version

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.6.7'
+
+setup(name='appy',
+      version=version,
+      description="Applications in Python",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[],
+      keywords='',
+      author='Gaetan Delannay',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False)
