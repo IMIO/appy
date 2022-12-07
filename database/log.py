921ccf7683ee9236e0748dc9fa46790c34f780f1
Gaetan Delannay <gaetan.delannay@geezteem.com>
2022-12-07 23:34:38 +0100

    [model > database > log] Viewer: added the possibility to view all log files: app.log, site.log or any backuped log file.

--- database/log.py
+++ database/log.py
@@ -116,6 +116,7 @@ class Config:
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 LOG_T_KO   = 'Unknown log type "%s".'
 LOG_M_KO   = 'Unknown log mode "%s".'
+LOG_F_KO   = 'File "%s" does not exist.'
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class Viewer:
@@ -137,17 +138,24 @@ class Viewer:
     # The main PX
     px = Px('''
      <!-- Controls -->
-     <form class="logControls" id="logForm">
+     <form class="logControls" id="logForm" var="logType=req.logType or 'app'">
 
       <!-- App or site log ? -->
-      <div var="logType=req.logType or 'app'">
+      <div>
        <input type="radio" value="app"  name="logType" id="app"
-               checked=":logType == 'app'"/>
+               checked=":logType == 'app'" onclick="refreshLogZone()"/>
        <label lfor="app">app</label>
        <input type="radio" value="site" name="logType" id="site"
-              checked=":logType == 'site'"/>
+              checked=":logType == 'site'" onclick="refreshLogZone()"/>
        <label lfor="site">site</label>
       </div>
+
+      <!-- Log file selector -->
+      <select name="logFile" var="files=viewer.listFiles(logType)"
+              onchange="refreshLogZone()">
+        <option for="id, name in files" value=":id"
+                selected=":req.logFile == id">:name</option>
+      </select>
 
       <!-- Refresh every x seconds -->
       <div class="textual">
@@ -203,9 +211,10 @@ class Viewer:
       }''',
 
      css='''.logControls { display:flex; align-items:center; gap:1em }
+            .logControls select { padding:0; margin:0; font-size:90% }
             .textual { display:flex; align-items:center; font-size:90%;
                        color:grey }
-            .logText { overflow:auto;width:65vw;height:65vh }
+            .logText { overflow:auto;width:65vw;height:65vh;font-size:90% }
             .editNB { margin:0 0.1em 0 0.3em; text-align:center }
             .rateR { width:2.5em }
             .tailN { width:4em; margin-right:0.4em }
@@ -272,14 +281,28 @@ class Viewer:
             if mode not in Viewer.modes:
                 r = LOG_M_KO % mode
             else:
-                # Get the log file's configuration object
-                cfg = getattr(tool.config.log, logType)
-                # Open the log file. If the file is not opened in binary mode,
-                # it is not possible to perfom seeks wwith a negative offset
-                # being relative to the end of the file.
-                with open(cfg.path, 'rb') as f:
-                    # Call the method corresponding to the mode
-                    r = getattr(self, mode)(f)
+                # Is that the current log file or a backup log file ?
+                fileName = req.logFile or 'app.log'
+                config = tool.config
+                if fileName.count('.') == 1 or \
+                   not fileName.startswith(logType + '.'):
+                    # It is the current log file: get its path from the config.
+                    # If the fileName does not correspond to the current log
+                    # type, this latter has probably been switched from the UI:
+                    # in that case, also get the current log file.
+                    path = getattr(config.log, logType).path
+                else:
+                    # A backup file
+                    path = config.backup.path / 'logs' / fileName
+                if not path.is_file():
+                    r = LOG_F_KO % str(path)
+                else:
+                    # Open the log file. If the file is not opened in binary
+                    # mode, it is not possible to perfom seeks wwith a negative
+                    # offset being relative to the end of the file.
+                    with open(path, 'rb') as f:
+                        # Call the method corresponding to the mode
+                        r = getattr(self, mode)(f)
         # Call the main PX
         context = O(viewer=self, tool=tool, req=req, svg=tool.buildSvg,
                     content=r)
@@ -288,6 +311,27 @@ class Viewer:
     def getAjaxData(self, c):
         '''Creates the Ajax data allowing to Ajax-refresh the log zone'''
         return "new AjaxData('%s', 'GET', null, '1_logsViewer')" % c.tool.url
+
+    def listFiles(self, logType):
+        '''Lists the existing log files of this p_logType'''
+        config = self.tool.config
+        # Start with the current log file
+        path = getattr(config.log, logType).path
+        current = (path.name, str(path))
+        r = None
+        # Add backup files if any
+        bconfig = config.backup
+        if bconfig and bconfig.path:
+            logFolder = bconfig.path / 'logs'
+            if logFolder.is_dir():
+                r = []
+                for logFile in logFolder.glob('%s.*.log' % logType):
+                    r.append((logFile.name, str(logFile)))
+                # Sort them in antichronological order
+                r.sort(key=lambda e: e[0], reverse=True)
+                # Insert the current log at the start of the list
+                r.insert(0, current)
+        return [current] if r is None else r
 
     @classmethod
     def run(class_, tool):
